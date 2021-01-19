734f855d562b17c0ff1dd0a25eb61f856461ed22
dmitry barinov <barinov3d@gmail.com>
2021-01-19 13:08:35 +0300

    Hot fix 201-add-sout-feature

--- src/main/java/org/barinov3d/Demo.java
+++ src/main/java/org/barinov3d/Demo.java
@@ -7,6 +7,7 @@ import org.barinov3d.service.DateService;
 public class Demo {
     public static void main(String[] args) {
         System.out.println(DateService.now());
+        System.out.println("Hello World!");
     }
 =======
 public class Demo {
